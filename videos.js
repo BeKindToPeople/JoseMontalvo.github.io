119201cd021be4f9cb1076da453cb78206f99e12
Jose Montalvo <montalvj4@gmail.com>
2025-12-21 13:35:17 -0800

    fixed the video error, latex pdf sizing and download error

--- videos.js
+++ videos.js
@@ -3,8 +3,8 @@ const lectureData = [
     title: "Lecture 0 - Test Video",
     description: "Testing the layout with a YouTube Short.",
     youtubeId: "hzFzAS8Dj6I", // Your Shorts ID
-    pdfLink: "#", // We can update this when you have the GitHub Raw link
-    pdfThumb: "OverLeafPDFs\OhRight.pdf" // Using an existing image as a placeholder for the PDF cover
+    pdfLink: "OverLeafPDFs/OhRight.pdf", // Local PDF in OverLeafPDFs folder
+    pdfThumb: "OverLeafPDFs/OhRight.pdf" // Using the actual PDF as a thumbnail source placeholder
   },
   // You can copy-paste this block to add Lecture 1, 2, etc.
 ];
