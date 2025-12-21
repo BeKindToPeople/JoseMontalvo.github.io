c3cdb52411d65bfc5825932a6caf1a82c39007fd
Jose Montalvo <montalvj4@gmail.com>
2025-12-21 13:20:56 -0800

    changed the portfolio layout, added test PDF and plumbing for youtube videos

---
+++ videos.js
@@ -0,0 +1,10 @@
+const lectureData = [
+  {
+    title: "Lecture 0 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "hzFzAS8Dj6I", // Your Shorts ID
+    pdfLink: "#", // We can update this when you have the GitHub Raw link
+    pdfThumb: "OverLeafPDFs\OhRight.pdf" // Using an existing image as a placeholder for the PDF cover
+  },
+  // You can copy-paste this block to add Lecture 1, 2, etc.
+];
