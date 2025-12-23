815baa04d1ee4c0aac7dcd6aabce4554718e823e
Jose Montalvo <montalvj4@gmail.com>
2025-12-23 11:22:54 -0800

    added rotational texx

--- videosRotational.js
+++ videosRotational.js
@@ -3,8 +3,8 @@ const lectureData = [
     title: "Lecture 0 - Test Video",
     description: "Testing the layout with a YouTube Short.",
     youtubeId: "21X5lGlDOfg", // Your Shorts ID
-    pdfLink: "Rotations/OverLeafPDFs/OhRight.pdf", // Local PDF in OverLeafPDFs folder
-    pdfThumb: "Rotations/OverLeafPDFs/OhRight.pdf" // Using the actual PDF as a thumbnail source placeholder
+    pdfLink: "OverLeafPDFs/Rotational/OhRight.pdf", // Local PDF in OverLeafPDFs folder
+    pdfThumb: "OverLeafPDFs/Rotations/OhRight.pdf" // Using the actual PDF as a thumbnail source placeholder
   },
   // You can copy-paste this block to add Lecture 1, 2, etc.
 ];
