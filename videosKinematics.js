7962a0e40c6f59432680c918930c1c6daa798eb7
Jose Montalvo <montalvj4@gmail.com>
2025-12-22 22:42:39 -0800

    kinematics page

---
+++ videosKinematics.js
@@ -0,0 +1,10 @@
+const lectureData = [
+  {
+    title: "Lecture 0 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/OhRight.pdf", // Local PDF in OverLeafPDFs folder
+    pdfThumb: "OverLeafPDFs/OhRight.pdf" // Using the actual PDF as a thumbnail source placeholder
+  },
+  // You can copy-paste this block to add Lecture 1, 2, etc.
+];
