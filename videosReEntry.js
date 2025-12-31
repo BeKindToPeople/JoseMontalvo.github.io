9121b2856103c97c07d931417c9f7316cf6f902a
Jose Montalvo <montalvj4@gmail.com>
2025-12-31 11:13:13 -0800

    webs

--- videosReEntry.js
+++ videosReEntry.js
@@ -1,6 +1,6 @@
 const lectureData = [
   {
-    title: "Lecture 0 - Test Video",
+    title: "Lecture 07 - Test Video",
     description: "Testing the layout with a YouTube Short.",
     youtubeId: "21X5lGlDOfg", // Your Shorts ID
     pdfLink:  "OverLeafPDFs/ReEntry/ReEntryMono.pdf", 
