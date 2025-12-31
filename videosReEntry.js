9aaccfed457979d50a08b0bd3ed92ae2c8d24f0f
Jose Montalvo <montalvj4@gmail.com>
2025-12-31 11:08:49 -0800

    pdf

--- videosReEntry.js
+++ videosReEntry.js
@@ -3,7 +3,7 @@ const lectureData = [
     title: "Lecture 0 - Test Video",
     description: "Testing the layout with a YouTube Short.",
     youtubeId: "21X5lGlDOfg", // Your Shorts ID
-    pdfLink: "OverLeafPDFs/ReEntry/ReEntryMono.pdf", 
+    pdfLink:  "OverLeafPDFs/ReEntry/ReEntryMono.pdf", 
     pdfThumb: "OverLeafPDFs/ReEntry/ReEntryMono.pdf" 
   },
   //copy-paste 
