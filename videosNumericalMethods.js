e68cfdb06225805cbaab2a480104290ed733ee7c
Jose Montalvo <montalvj4@gmail.com>
2026-01-03 11:15:15 -0800

    NumMain

--- videosNumericalMethods.js
+++ videosNumericalMethods.js
@@ -3,20 +3,20 @@ const lectureData = [
     title: "Lecture 0 - Test Video",
     description: "Testing the layout with a YouTube Short.",
     youtubeId: "21X5lGlDOfg", 
-    pdfLink: "OverLeafPDFs/NumericalMethods/00/NumericalMethodsMono00.pdf", 
+    pdfLink: "OverLeafPDFs/NumericalMethods/NumericalMethodsMono.pdf", 
   },
 
   {
     title: "Lecture 1 - Test Video",
     description: "Testing the layout with a YouTube Short.",
     youtubeId: "21X5lGlDOfg", 
-    pdfLink: "OverLeafPDFs/NumericalMethods/01/NumericalMethodsMono01.pdf", 
+    pdfLink: "OverLeafPDFs/NumericalMethods/NumericalMethodsMono.pdf", 
   },
 
   {
     title: "Lecture 2 - Test Video", 
     description: "Testing the layout with a YouTube Short.",
     youtubeId: "21X5lGlDOfg", 
-    pdfLink: "OverLeafPDFs/NumericalMethods/02/NumericalMethodsMono02.pdf", 
+    pdfLink: "OverLeafPDFs/NumericalMethods/NumericalMethodsMono.pdf", 
   },
 ];
