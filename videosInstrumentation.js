a9e62e4335985fc497720ad32db59e807fa9984e
Jose Montalvo <montalvj4@gmail.com>
2026-04-21 18:04:59 -0700

    rest of inst

--- videosInstrumentation.js
+++ videosInstrumentation.js
@@ -14,5 +14,73 @@ const lectureData = [
     pdfLink: "OverLeafPDFs/Instrumentation/Lab2.pdf", 
     pdfThumb: "OverLeafPDFs/Instrumentation/Lab2.pdf" 
   },
+
+
+  {
+    title: "Lab 3 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab3.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab3.pdf" 
+  },
+
+  {
+    title: "Lab 4 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab4.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab4 .pdf" 
+  },
+
+  {
+    title: "Lab 5 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab5.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab5.pdf" 
+  },
+
+  {
+    title: "Lab 61 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab61.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab61.pdf" 
+  },
+
+  {
+    title: "Lab 62 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab62.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab62.pdf" 
+  },
+
+  {
+    title: "Lab 71 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab71.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab71.pdf" 
+  },
+
+  {
+    title: "Lab 72 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab72.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab72.pdf" 
+  },
+
+
+  {
+    title: "Lab 8 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab8.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab8.pdf" 
+  },
+
+  
   // You can copy-paste
 ];
