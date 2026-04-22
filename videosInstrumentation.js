58a417cc9c2413c37d70ff1630184a849bec22ea
Jose Montalvo <montalvj4@gmail.com>
2026-04-21 17:01:12 -0700

    instrumentation section

---
+++ videosInstrumentation.js
@@ -0,0 +1,10 @@
+const lectureData = [
+  {
+    title: "Lecture 0 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Capstone/designCapstone.pdf", 
+    pdfThumb: "OverLeafPDFs/Capstone/designCapstone.pdf" 
+  },
+  // You can copy-paste
+];
