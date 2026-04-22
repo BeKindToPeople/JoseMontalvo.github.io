45ee7076e825d449fb9f68ae30db21e924bd652b
Jose Montalvo <montalvj4@gmail.com>
2026-04-21 17:30:49 -0700

    instLab1

--- videosInstrumentation.js
+++ videosInstrumentation.js
@@ -1,10 +1,18 @@
 const lectureData = [
   {
-    title: "Lecture 0 - Test Video",
+    title: "Lab 1 - Test Video",
     description: "Testing the layout with a YouTube Short.",
     youtubeId: "21X5lGlDOfg", // Your Shorts ID
     pdfLink: "OverLeafPDFs/Instrumentation/Lab1.pdf", 
     pdfThumb: "OverLeafPDFs/Instrumentation/Lab1.pdf" 
+  },
+
+  {
+    title: "Lab 2 - Test Video",
+    description: "Testing the layout with a YouTube Short.",
+    youtubeId: "21X5lGlDOfg", // Your Shorts ID
+    pdfLink: "OverLeafPDFs/Instrumentation/Lab2.pdf", 
+    pdfThumb: "OverLeafPDFs/Instrumentation/Lab2.pdf" 
   },
   // You can copy-paste
 ];
